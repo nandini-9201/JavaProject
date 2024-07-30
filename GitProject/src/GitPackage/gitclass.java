88929948c6b2509746a77656034f1b2db5b9435c
nandini <nandini.nanda.s85@gmail.com>
2024-07-30 10:39:45 +0530

    First Commit

---
+++ GitProject/src/GitPackage/gitclass.java
@@ -0,0 +1,12 @@
+package GitPackage;
+
+public class gitclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a = 20;
+int b = 30;
+System.out.println("Sum ="+(a+b));
+	}
+
+}
